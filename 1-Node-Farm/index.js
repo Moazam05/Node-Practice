42a970897816fdd40d52b7d4b00d682b1d10b6d5
Moazam05 <salmanmoazam08@gmail.com>
2023-08-26 14:39:49 +0500

    file system asynchronous way

--- 1-Node-Farm/index.js
+++ 1-Node-Farm/index.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 
+console.log("======= Blocking, synchronous way =======");
+// Blocking, synchronous way
+
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
 
@@ -7,3 +10,24 @@ const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on 
 
 fs.writeFileSync("./txt/output.txt", textOut);
 console.log("File Written!");
+
+console.log("======= Non-Blocking, asynchronous way =======");
+// Non-Blocking, asynchronous way
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+	if (err) return console.log("Error 💥");
+	// console.log(data1);
+	fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+		console.log(data2);
+		fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+			console.log(data3);
+
+			// Write File
+			fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+				console.log("Your file has been written 😊");
+			});
+		});
+	});
+});
+
+console.log("Will Read File!");
