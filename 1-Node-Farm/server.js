afec7f7ee3c6fe937f1d3d5bcedc9e51f1983991
Moazam05 <salmanmoazam08@gmail.com>
2023-09-01 15:58:27 +0500

    slugify install

--- 1-Node-Farm/server.js
+++ 1-Node-Farm/server.js
@@ -2,6 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 const replaceTemplate = require("./modules/replaceTemplate");
+const slugify = require("slugify");
 
 const tempOverview = fs.readFileSync(
 	`${__dirname}/templates/template-overview.html`,
@@ -18,6 +19,9 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
 	const { query, pathname } = url.parse(req.url, true);
