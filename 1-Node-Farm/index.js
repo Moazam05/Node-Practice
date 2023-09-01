9bdde5875767c7be17243d51bafd856696595dc4
Moazam05 <salmanmoazam08@gmail.com>
2023-09-01 16:20:05 +0500

    prettier configure

--- 1-Node-Farm/index.js
+++ 1-Node-Farm/index.js
@@ -1,33 +1,33 @@
-const fs = require("fs");
+const fs = require('fs');
 
-console.log("======= Blocking, synchronous way =======");
+console.log('======= Blocking, synchronous way =======');
 // Blocking, synchronous way
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 console.log(textIn);
 
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File Written!");
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File Written!');
 
-console.log("======= Non-Blocking, asynchronous way =======");
+console.log('======= Non-Blocking, asynchronous way =======');
 // Non-Blocking, asynchronous way
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-	if (err) return console.log("Error 💥");
-	// console.log(data1);
-	fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-		console.log(data2);
-		fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-			console.log(data3);
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('Error 💥');
+  // console.log(data1);
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+      console.log(data3);
 
-			// Write File
-			fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-				console.log("Your file has been written 😊");
-			});
-		});
-	});
+      // Write File
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        console.log('Your file has been written 😊');
+      });
+    });
+  });
 });
 
-console.log("Will Read File!");
+console.log('Will Read File!');
--- 1-Node-Farm/server.js
+++ 1-Node-Farm/server.js
@@ -1,70 +1,70 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
-const replaceTemplate = require("./modules/replaceTemplate");
-const slugify = require("slugify");
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const replaceTemplate = require('./modules/replaceTemplate');
+const slugify = require('slugify');
 
 const tempOverview = fs.readFileSync(
-	`${__dirname}/templates/template-overview.html`,
-	"utf-8"
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
 );
 const tempCard = fs.readFileSync(
-	`${__dirname}/templates/template-card.html`,
-	"utf-8"
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
 );
 const tempProduct = fs.readFileSync(
-	`${__dirname}/templates/template-product.html`,
-	"utf-8"
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
 );
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
 console.log(slugs);
 
 const server = http.createServer((req, res) => {
-	const { query, pathname } = url.parse(req.url, true);
+  const { query, pathname } = url.parse(req.url, true);
 
-	// Overview Page
-	if (pathname === "/" || pathname === "/overview") {
-		// res.end("This is the OVERVIEW");
+  // Overview Page
+  if (pathname === '/' || pathname === '/overview') {
+    // res.end("This is the OVERVIEW");
 
-		res.writeHead(200, { "Content-type": "text/html" });
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-		const cardHtml = dataObj
-			.map((el) => replaceTemplate(tempCard, el))
-			.join("");
-		const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardHtml);
+    const cardHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHtml);
 
-		res.end(output);
+    res.end(output);
 
-		// Product Page
-	} else if (pathname === "/product") {
-		// res.end("This is the PRODUCT");
+    // Product Page
+  } else if (pathname === '/product') {
+    // res.end("This is the PRODUCT");
 
-		res.writeHead(200, { "Content-type": "text/html" });
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-		const product = dataObj[query.id];
-		const output = replaceTemplate(tempProduct, product);
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
 
-		res.end(output);
+    res.end(output);
 
-		// API Page
-	} else if (pathname === "/api") {
-		fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
-			// const productData = JSON.parse(data);
-			// console.log(productData);
-			// res.writeHead(200, { "Content-type": "application/json" });
-			res.end(data);
-		});
+    // API Page
+  } else if (pathname === '/api') {
+    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+      // const productData = JSON.parse(data);
+      // console.log(productData);
+      // res.writeHead(200, { "Content-type": "application/json" });
+      res.end(data);
+    });
 
-		// Not found
-	} else {
-		res.end("Page not found!");
-	}
+    // Not found
+  } else {
+    res.end('Page not found!');
+  }
 });
 
-server.listen(8000, "127.0.0.1", () => {
-	console.log("Listening to requests on port 8000");
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
 });
