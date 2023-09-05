e51bd04c90c74ff2e493cb733d49a25a658b5234
Moazam05 <salmanmoazam08@gmail.com>
2023-09-05 21:32:58 +0500

    delete tour

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -48,7 +48,6 @@ app.get('/api/v1/tours/:id', (req, res) => {
 // Update tour
 app.patch('/api/v1/tours/:id', (req, res) => {
   const id = Number(req.params.id);
- 
 
   if (id > tours.length)
     return res.status(404).json({
@@ -83,6 +82,22 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+// Delete Single Tour
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  if (id > tours.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(204).json({
+    status: 'success',
+    message: null,
+  });
 });
 
 const port = 3000;
