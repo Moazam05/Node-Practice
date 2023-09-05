70abe0627a65385d581c4c46ddfa9b3f6ffaa937
Moazam05 <salmanmoazam08@gmail.com>
2023-09-05 21:24:47 +0500

    update single tour

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -45,6 +45,25 @@ app.get('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+// Update tour
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+ 
+
+  if (id > tours.length)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
 // Create New Tour
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
