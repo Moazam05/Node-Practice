87588c1a616efb4f580da6f06af6aeff730e93b8
Moazam05 <salmanmoazam08@gmail.com>
2023-09-06 14:40:23 +0500

    static files view using express

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -5,12 +5,11 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-// 1 MIDDLEWARE
 const app = express();
-
+// 1 MIDDLEWARE
 app.use(morgan('dev'));
-
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware 👋🏻');
