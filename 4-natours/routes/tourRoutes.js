3c255b388f5d73a2e81bc58e6326d10c958bda7d
Moazam05 <salmanmoazam08@gmail.com>
2023-09-18 14:55:22 +0500

    top 5 cheap

--- 4-natours/routes/tourRoutes.js
+++ 4-natours/routes/tourRoutes.js
@@ -9,12 +9,15 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  aliasTopTours,
   // checkID,
 } = tourController;
 
 const router = express.Router();
 
 // router.param('id', checkID);
+
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
