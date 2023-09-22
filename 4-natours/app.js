8c77ff19245cb37c62980e492410fc591d0e1463
Moazam05 <salmanmoazam08@gmail.com>
2023-09-22 17:25:46 +0500

    global error

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 // Custom Imports
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 // 1 MIDDLEWARE
@@ -38,10 +40,15 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
+
+  // next(err);
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
