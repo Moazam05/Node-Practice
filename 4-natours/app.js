34b3c519925d9b15a0486386a01965d576c3a918
Moazam05 <salmanmoazam08@gmail.com>
2023-10-17 14:53:53 +0500

    no sql sanitization and xss prevent

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const dotenv = require('dotenv');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 // Custom Imports
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -33,6 +35,12 @@ app.use('/api', limiter);
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
 
@@ -46,23 +54,11 @@ app.get('/', (req, res) => {
   res.send('Natours API is running...');
 });
 
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
 // 3 ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  // const err = new Error(`Can't find ${req.originalUrl} on this server!`);
-  // err.statusCode = 404;
-  // err.status = 'fail';
-
-  // next(err);
-
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
