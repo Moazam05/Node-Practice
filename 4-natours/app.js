89f0be396ca2792a3e23a3a0f649c05b32eeb7bb
Moazam05 <salmanmoazam08@gmail.com>
2023-10-17 14:17:41 +0500

    request per hour, jwt in cookies

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,6 +1,7 @@
 // 3rd party modules
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 // Custom Imports
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -8,10 +9,18 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
-// 1 MIDDLEWARE
+// 1 GLOBAL MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: process.env.API_REQUEST_PER_HOUR,
+  windowMs: 60 * 60 * 1000,
+  message: 'To many request from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
