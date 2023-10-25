d308f48a52ad26c83a65ffa0365608ceb9c906a7
Moazam05 <salmanmoazam08@gmail.com>
2023-10-25 20:10:53 +0500

    get factory function

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -176,22 +176,8 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id).populate('reviews');
-  // Tour.findOne({ _id: req.params.id })
-
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404));
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-});
-
+exports.getTour = factory.getOne(Tour, { path: 'reviews' });
+// const tour = await Tour.findById(req.params.id).populate('reviews');
 exports.updateTour = factory.updateOne(Tour);
 exports.createTour = factory.createOne(Tour);
 exports.deleteTour = factory.deleteOne(Tour);
