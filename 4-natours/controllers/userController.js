b2b82aa77e0d9b9a7ad61f9b9798b69006b57762
Moazam05 <salmanmoazam08@gmail.com>
2023-10-26 11:42:11 +0500

    get me

--- 4-natours/controllers/userController.js
+++ 4-natours/controllers/userController.js
@@ -11,6 +11,18 @@ const filterObj = (obj, ...allowedFields) => {
   return newObject;
 };
 
+exports.createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined! Please use /signup instead',
+  });
+};
+
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.updateMe = catchAsync(async (req, res, next) => {
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
@@ -47,13 +59,6 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
     data: null,
   });
 });
-
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined! Please use /signup instead',
-  });
-};
 
 exports.getAllUsers = factory.getAll(User);
 exports.getUser = factory.getOne(User);
