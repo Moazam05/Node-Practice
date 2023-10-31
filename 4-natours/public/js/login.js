13f51fc5a09bd8edcdc54c4f8e44a5dc9ce7fc02
Moazam05 <salmanmoazam08@gmail.com>
2023-10-31 17:17:36 +0500

    alert message

--- 4-natours/public/js/login.js
+++ 4-natours/public/js/login.js
@@ -1,3 +1,17 @@
+const hideAlert = () => {
+  const el = document.querySelector('.alert');
+  if (el) el.parentElement.removeChild(el);
+};
+
+const showAlert = (type, msg) => {
+  hideAlert();
+
+  const markup = `<div class="alert alert--${type}">${msg}</div>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+
+  window.setTimeout(hideAlert, 5000);
+};
+
 const login = async (email, password) => {
   try {
     const res = await axios({
@@ -10,13 +24,13 @@ const login = async (email, password) => {
     });
 
     if (res.data.status === 'success') {
-      alert('Logged in successfully!');
+      showAlert('success', 'Logged in successfully!');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (err) {
-    alert(err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
 
